e081426e87934fe6a29286271192f31bc57992af
brittain9 <alexbrittain9@gmail.com>
2024-07-27 12:02:10 -0500

    multilingual 2

--- src/WikiQuizGenerator.Core/Models/WikipediaPage.cs
+++ src/WikiQuizGenerator.Core/Models/WikipediaPage.cs
@@ -4,20 +4,22 @@ using System.Text;
 
 namespace WikiQuizGenerator.Core.Models;
 
-// Stores the info from specific query in GetWikipediaPage
 public class WikipediaPage
 {
     public int Id { get; set; } // pageid
+    public string Langauge {get; set;} // pagelangauge
     public string Title { get; set; } // title
     public string Extract { get; set; } // extract
     public DateTime LastModified { get; set; }  // touched
     public string Url { get; set; } // fullurl
     public int Length { get; set; } // length, seems to be response char count
     public List<string> Links { get; set; } // links
+    public List<string> Categories { get; set; }
 
     public WikipediaPage()
     {
         Links = new List<string>();
+        Categories = new List<string>();
     }
 
     public override string ToString()
@@ -25,20 +27,14 @@ public class WikipediaPage
         StringBuilder sb = new StringBuilder();
 
         sb.AppendLine($"Wikipedia Article: {Title}");
-        sb.AppendLine(new string('=', Title.Length + 19)); // Underline the title
 
         sb.AppendLine($"\nURL: {Url}");
         sb.AppendLine($"Last Modified: {LastModified:yyyy-MM-dd HH:mm:ss}");
 
         sb.AppendLine("\nContent:");
-        sb.AppendLine(new string('-', 8)); // Underline "Content:"
         sb.AppendLine(Extract.Length > 100 ? Extract.Substring(0, 100) + "..." : Extract);
-
-        sb.AppendLine("\nCategories:");
-        sb.AppendLine(new string('-', 11)); // Underline "Categories:"
-
+        
         sb.AppendLine("\nRelated Links:");
-        sb.AppendLine(new string('-', 14)); // Underline "Related Links:"
         foreach (var link in Links.Take(10)) // Limit to first 10 related links
         {
             sb.AppendLine($"- {link}");
--- src/WikiQuizGenerator.Core/WikipediaContent.cs
+++ src/WikiQuizGenerator.Core/WikipediaContent.cs
@@ -1,81 +1,59 @@
 using System;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Text.Json;
 using System.Collections.Generic;
 
 using WikiQuizGenerator.Core.Models;
+using WikiQuizGenerator.Core;
 
 public class WikipediaContent
 {
-    private static readonly HttpClient client = new HttpClient();
-    private const string API_ENDPOINT = "https://en.wikipedia.org/w/api.php";
-
-    /// <summary>
-    /// Removes HTML formatting and cleans up the input string.
-    /// </summary>
-    /// <param name="input">The string to be cleaned.</param>
-    /// <returns>A cleaned string without HTML tags and extra whitespace.</returns>
-    public static string RemoveFormatting(string input)
-    {
-        if (string.IsNullOrEmpty(input))
-        {
-            return input;
-        }
-
-        // Remove all HTML tags
-        input = Regex.Replace(input, @"<[^>]+>", string.Empty);
-
-        // Remove extra whitespace
-        input = Regex.Replace(input, @"\s+", " ");
-
-        // Decode HTML entities
-        input = System.Net.WebUtility.HtmlDecode(input);
-
-        return input.Trim();   
-    }
-
     /// <summary>
     /// Fetches the content of a Wikipedia article based on the given topic.
     /// </summary>
     /// <param name="topic">The topic to search for on Wikipedia.</param>
     /// <returns>A WikipediaArticle object containing the article information.</returns>
-    public static async Task<WikipediaPage> GetWikipediaPage(string topic)
+    public static async Task<WikipediaPage> GetWikipediaPage(string topic, string language = "en")
     {
-        // I want to differiate between a valid page and Disambiguation pages (as I just found out they are called)
+        HttpClient client = new HttpClient();
+
+        string API_ENDPOINT = $"https://{language}.wikipedia.org/w/api.php";
+
 
         var query = HttpUtility.UrlEncode(topic);
 
-        // query properties full extract, info (url and page info), up to 500 links
-        // links are in alphabetically ordered. Query lots of them and we can randomly select.
-        var url = $"{API_ENDPOINT}?action=query&format=json&prop=extracts|info|links&redirects=1&inprop=url|displaytitle&pllimit=100&titles={query}";
+        // Construct the API URL with the specified language, get the extract, general info, links, and categories
+        var url = $"{API_ENDPOINT}?action=query&format=json&prop=extracts|info|links|categories&redirects=1&inprop=url|displaytitle&pllimit=100&titles={query}";
 
+        // This code is ugly but it works for now.
         try
         {
             var response = await client.GetStringAsync(url);
             var jsonDoc = JsonDocument.Parse(response);
             var pages = jsonDoc.RootElement.GetProperty("query").GetProperty("pages");
 
-            // get the first valid page of our query
+            // Get the first valid page of our query
             foreach (var page in pages.EnumerateObject())
             {
                 var wikiPage = new WikipediaPage
                 {
+                    // TODO: Fix these null reference assignment warnings
                     Id = page.Value.GetProperty("pageid").GetInt32(),
                     Title = page.Value.GetProperty("title").GetString(),
-                    Extract = RemoveFormatting(page.Value.GetProperty("extract").GetString()),
+                    Extract = Utility.RemoveFormatting(page.Value.GetProperty("extract").GetString()),
                     LastModified = DateTime.Parse(page.Value.GetProperty("touched").GetString()),
                     Url = page.Value.GetProperty("fullurl").GetString(),
                     Length = page.Value.GetProperty("length").GetInt32()
                 };
 
+
                 if (page.Value.TryGetProperty("links", out var links))
                 {
                     foreach (var link in links.EnumerateArray())
                     {
-                        // could do every other link to reduce size
+                        // Could do every other link to reduce size
                         wikiPage.Links.Add(link.GetProperty("title").GetString());
                     }
                 }
