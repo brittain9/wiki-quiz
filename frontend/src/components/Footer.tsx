78b5b294d4b5fc8abaea0680bb97c48b945eeac1
brittain9 <alexbrittain9@gmail.com>
2025-07-09 14:49:58 -0500

    backend workflow container name env

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,7 +9,6 @@ import { useTranslation } from 'react-i18next';
 
 const Footer = React.memo(() => {
   const { t } = useTranslation();
-  // dummy change to test the pipeline
   return (
     <Box
       component="footer"
